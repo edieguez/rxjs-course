edf6bc3ab928d0ee6833aab04109df924c09069e
edieguez <dieguezemmanuel@gmail.com>
2025-06-26 09:00:36 -0600

    Add courses filtering

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,13 @@
+import {Observable} from "rxjs";
 
-
+export function createHttpObservable(url: string) {
+  return new Observable(observer => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        observer.next(data);
+        observer.complete();
+      })
+      .catch(error => observer.error(error));
+  });
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,39 @@
 import {Component, OnInit} from '@angular/core';
+import {createHttpObservable} from "../common/util";
+import {map} from "rxjs/operators";
 import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    standalone: false
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  standalone: false
 })
 export class HomeComponent implements OnInit {
+  private url: string = 'http://localhost:9000/api/courses'
 
+  beginnerCourses: Course[];
+  advancedCourses: Course[];
 
-    constructor() {
-
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+  ngOnInit() {
+    const http$ = createHttpObservable(this.url);
+    http$.pipe(
+      map(data => Object.values(data['payload'])),
+      map(objects => objects.map(
+        (object) => ({
+          id: object['id'],
+          description: object['description'],
+          iconUrl: object['iconUrl'],
+          courseListIcon: object['courseListIcon'],
+          longDescription: object['longDescription'],
+          category: object['category'],
+          lessonsCount: object['lessonsCount'],
+        } as Course)
+      ))
+    ).subscribe(courses => {
+      this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
+      this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
+    });
+  }
 }
